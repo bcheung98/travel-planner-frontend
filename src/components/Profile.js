82f4bf7b6cb7b7a77288343cf77a8fdf59eae4a0
Brian <briantcheung@gmail.com>
2021-05-21 10:21:33 -0700

    Added destinations to user profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,15 +1,38 @@
 import { Typography } from "@material-ui/core";
-import React from "react";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
 import { withRouter } from "react-router";
+import { showMyDestinations } from "../redux/actions/showMyDestinations";
+import DestinationList from "./DestinationList";
 
 const Profile = (props) => {
+
+    let { destinations, showMyDestinations } = props
+
+    useEffect(() => {
+        showMyDestinations();
+    }, [])
+
     return (
         <div>
             <Typography>
                 My Profile
             </Typography>
+            <DestinationList destinations={destinations} />
         </div>
     )
 }
 
-export default withRouter(Profile);
+const mapStateToProps = (state) => {
+    return {
+        destinations: state.destination
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        showMyDestinations: () => dispatch(showMyDestinations())
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(withRouter(Profile));
--- src/redux/reducers/destinationReducer.js
+++ src/redux/reducers/destinationReducer.js
@@ -17,6 +17,19 @@ const destinationReducer = (state = initialState, action) => {
                 destinations: action.destinations,
                 requesting: false
             }
+        case "START_GETTING_USER_DESTINATIONS_REQUEST":
+            return {
+                ...state,
+                destinations: [...state.destinations],
+                requesting: true
+            }
+        case "GET_USER_DESTINATIONS":
+            console.log(action.destinations)
+            return {
+                ...state,
+                destinations: action.destinations,
+                requesting: false
+            }
         case "START_ADDING_DESTINATIONS_REQUEST":
             return {
                 ...state,
@@ -24,7 +37,6 @@ const destinationReducer = (state = initialState, action) => {
                 requesting: true
             }
         case "ADD_DESTINATION":
-            console.log(action.payload)
             return state
         default:
             return state
