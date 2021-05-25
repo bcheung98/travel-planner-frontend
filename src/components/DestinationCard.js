cbec2fb46c6b767997115a74437417888c4dc2cd
Brian <briantcheung@gmail.com>
2021-05-25 13:25:27 -0700

    Added accordion cards for destinations

--- src/components/DestinationCard.js
+++ src/components/DestinationCard.js
@@ -1,44 +1,52 @@
 import React from "react";
 
 import { makeStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
+import Accordion from '@material-ui/core/Accordion';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionActions from '@material-ui/core/AccordionActions';
 import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import Button from '@material-ui/core/Button';
+import Divider from '@material-ui/core/Divider';
 import { connect } from "react-redux";
 import { addDestination } from "../redux/actions/addDestination";
 import { deleteDestination } from "../redux/actions/deleteDestination";
 
 const useStyles = makeStyles((theme) => ({
     root: {
-        flexGrow: 1,
-    },
-    paper: {
-        border: "2px solid black",
-        padding: theme.spacing(2),
-        margin: 'auto',
+        width: 750,
+        margin: "auto",
         marginBottom: 20,
-        maxWidth: 500,
     },
-    image: {
-        width: 128,
-        height: 128,
+    heading: {
+        fontSize: theme.typography.pxToRem(15),
     },
-    img: {
-        margin: 'auto',
-        display: 'block',
-        maxWidth: '100%',
-        maxHeight: '100%',
+    secondaryHeading: {
+        fontSize: theme.typography.pxToRem(15),
+        color: theme.palette.text.secondary,
     },
-    buttons: {
-        margin: theme.spacing(2),
+    icon: {
+        verticalAlign: 'bottom',
+        height: 20,
+        width: 20,
     },
-    formControl: {
-        margin: theme.spacing(3),
-        minWidth: 100,
+    details: {
+        alignItems: 'center',
     },
-    selectEmpty: {
-        marginTop: theme.spacing(2),
+    column: {
+        flexBasis: '33.33%',
+    },
+    helper: {
+        borderLeft: `2px solid ${theme.palette.divider}`,
+        padding: theme.spacing(1, 2),
+    },
+    link: {
+        color: theme.palette.primary.main,
+        textDecoration: 'none',
+        '&:hover': {
+            textDecoration: 'underline',
+        },
     },
 }));
 
@@ -46,24 +54,35 @@ const DestinationCard = (props) => {
     const classes = useStyles();
     let { name, location, country } = props.destination
     return (
-        <Paper className={classes.paper}>
-            <Typography variant="h4">
-                {name}
-            </Typography>
-            <Typography variant="subtitle1">
-                {location}, {country}
-            </Typography>
-            {
-                window.location.pathname === "/browse" ?
-                    <Button onClick={() => props.add_destination(props.destination)} className={classes.buttons}>
-                        +
-                    </Button>
-                    :
-                    <Button onClick={() => props.delete_destination(props.destination)} className={classes.buttons}>
-                        -
-                    </Button>
-            }
-        </Paper>
+        <div className={classes.root}>
+            <Accordion>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1c-content"
+                    id="panel1c-header"
+                >
+                    <div className={classes.column}>
+                        <Typography className={classes.heading}>{name}</Typography>
+                    </div>
+                    <div className={classes.column}>
+                        <Typography className={classes.secondaryHeading}>{location}, {country}</Typography>
+                    </div>
+                </AccordionSummary>
+                <AccordionDetails className={classes.details}>
+                    <div className={classes.column} />
+                </AccordionDetails>
+                <Divider />
+                <AccordionActions>
+                    <Button size="small">Cancel</Button>
+                    {
+                        window.location.pathname === "/browse" ?
+                            <Button onClick={() => props.add_destination(props.destination)} size="small" color="primary">Add Destination</Button>
+                            :
+                            <Button onClick={() => props.delete_destination(props.destination)} size="small" color="secondary">Remove Destination</Button>
+                    }
+                </AccordionActions>
+            </Accordion>
+        </div>
     )
 }
 
