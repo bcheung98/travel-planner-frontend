c8f23ad68f380a6a2b24acb3a223c76297966a77
Brian <briantcheung@gmail.com>
2021-05-25 21:29:23 -0700

    Made destinations into cards

--- src/components/DestinationCard.js
+++ src/components/DestinationCard.js
@@ -1,88 +1,59 @@
 import React from "react";
 
 import { makeStyles } from '@material-ui/core/styles';
-import Accordion from '@material-ui/core/Accordion';
-import AccordionDetails from '@material-ui/core/AccordionDetails';
-import AccordionSummary from '@material-ui/core/AccordionSummary';
-import AccordionActions from '@material-ui/core/AccordionActions';
-import Typography from '@material-ui/core/Typography';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
-import Divider from '@material-ui/core/Divider';
+import Typography from '@material-ui/core/Typography';
 import { connect } from "react-redux";
 import { addDestination } from "../redux/actions/addDestination";
 import { deleteDestination } from "../redux/actions/deleteDestination";
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles({
     root: {
-        width: 750,
         margin: "auto",
         marginBottom: 20,
+        maxWidth: 500,
+        border: "2px solid gray"
     },
-    heading: {
-        fontWeight: "bold",
-        fontSize: theme.typography.pxToRem(15),
+    media: {
+        height: 650
     },
-    secondaryHeading: {
-        fontSize: theme.typography.pxToRem(15),
-        color: theme.palette.text.secondary,
-    },
-    icon: {
-        verticalAlign: 'bottom',
-        height: 20,
-        width: 20,
-    },
-    details: {
-        alignItems: 'center',
-    },
-    column: {
-        flexBasis: '33.33%',
-    },
-    helper: {
-        borderLeft: `2px solid ${theme.palette.divider}`,
-        padding: theme.spacing(1, 2),
-    },
-    link: {
-        color: theme.palette.primary.main,
-        textDecoration: 'none',
-        '&:hover': {
-            textDecoration: 'underline',
-        },
-    },
-}));
+});
 
 const DestinationCard = (props) => {
     const classes = useStyles();
-    let { name, location, country } = props.destination
+    let { name, location, country, image, description } = props.destination;
     return (
-        <div className={classes.root}>
-            <Accordion>
-                <AccordionSummary
-                    expandIcon={<ExpandMoreIcon />}
-                    aria-controls="panel1c-content"
-                    id="panel1c-header"
-                >
-                    <div className={classes.column}>
-                        <Typography className={classes.heading}>{name}</Typography>
-                    </div>
-                    <div className={classes.column}>
-                        <Typography className={classes.secondaryHeading}>{location}, {country}</Typography>
-                    </div>
-                </AccordionSummary>
-                <AccordionDetails className={classes.details}>
-                    <div className={classes.column} />
-                </AccordionDetails>
-                <Divider />
-                <AccordionActions>
-                    {
-                        window.location.pathname === "/browse" ?
-                            <Button onClick={() => props.add_destination(props.destination)} size="small" color="primary">Add Destination</Button>
-                            :
-                            <Button onClick={() => props.delete_destination(props.destination)} size="small" color="secondary">Remove Destination</Button>
-                    }
-                </AccordionActions>
-            </Accordion>
-        </div>
+        <Card className={classes.root}>
+            <CardHeader
+                title={
+                    <Typography gutterBottom variant="h5" component="h2">{name}</Typography>
+                }
+                subheader={
+                    <Typography variant="body2" component="p">{location}, {country}</Typography>
+                }
+            />
+            <CardMedia
+                className={classes.media}
+                image={image}
+                title={name}
+            />
+            <CardContent>
+                <Typography variant="body2" component="p">{description}</Typography>
+            </CardContent>
+            <CardActions>
+                {
+                    window.location.pathname === "/browse" ?
+                        <Button onClick={() => props.add_destination(props.destination)} variant="contained" size="small" color="primary">Add Destination</Button>
+                        :
+                        <Button onClick={() => props.delete_destination(props.destination)} variant="contained" size="small" color="secondary">Remove Destination</Button>
+                }
+            </CardActions>
+        </Card>
     )
 }
 
