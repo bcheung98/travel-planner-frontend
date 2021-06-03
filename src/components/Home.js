429614e54e900f44551bbd97bdcc2d69444eb692
Brian <briantcheung@gmail.com>
2021-06-03 10:39:11 -0700

    Added image slideshow to homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,120 @@
+import React from 'react';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import MobileStepper from '@material-ui/core/MobileStepper';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+import SwipeableViews from 'react-swipeable-views';
+import { autoPlay } from 'react-swipeable-views-utils';
+
+const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
+
+const images = [
+    {
+        label: 'Golden Gate Bridge, United States',
+        imgPath:
+            'https://upload.wikimedia.org/wikipedia/commons/0/0c/GoldenGateBridge-001.jpg',
+    },
+    {
+        label: 'Angkor Wat, Cambodia',
+        imgPath:
+            'https://upload.wikimedia.org/wikipedia/commons/4/44/Ankor_Wat_temple.jpg',
+    },
+    {
+        label: 'Brandenburg Gate, Germany',
+        imgPath:
+            'https://upload.wikimedia.org/wikipedia/commons/a/a6/Brandenburger_Tor_abends.jpg',
+    },
+    {
+        label: 'Mount Fuji, Japan',
+        imgPath:
+            'https://upload.wikimedia.org/wikipedia/commons/1/1b/080103_hakkai_fuji.jpg',
+    },
+    {
+        label: 'Taj Mahal, India',
+        imgPath:
+            'https://upload.wikimedia.org/wikipedia/commons/6/67/Taj_Mahal_in_India_-_Kristian_Bertel.jpg',
+    },
+];
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        maxWidth: 1920,
+        flexGrow: 1,
+    },
+    header: {
+        textAlign: 'center',
+        height: 50,
+        paddingTop: theme.spacing(2),
+        backgroundColor: theme.palette.background.default,
+    },
+    img: {
+        height: "100vh",
+        width: '100%',
+        display: 'block',
+        overflow: 'hidden',
+    },
+}));
+
 const Home = () => {
+    const classes = useStyles();
+    const theme = useTheme();
+    const [activeStep, setActiveStep] = React.useState(0);
+    const maxSteps = images.length;
+
+    const handleNext = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    };
+
+    const handleBack = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    };
+
+    const handleStepChange = (step) => {
+        setActiveStep(step);
+    };
+
     return (
-        <div>
-            
+        <div className={classes.root}>
+            <AutoPlaySwipeableViews
+                axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+                index={activeStep}
+                onChangeIndex={handleStepChange}
+                enableMouseEvents
+            >
+                {images.map((step, index) => (
+                    <div key={step.label}>
+                        {Math.abs(activeStep - index) <= 2 ? (
+                            <img className={classes.img} src={step.imgPath} alt={step.label} />
+                        ) : null}
+                    </div>
+                ))}
+            </AutoPlaySwipeableViews>
+            <Paper square elevation={0} className={classes.header}>
+                <Typography>{images[activeStep].label}</Typography>
+            </Paper>
+            <MobileStepper
+                steps={maxSteps}
+                position="static"
+                variant="dots"
+                activeStep={activeStep}
+                nextButton={
+                    <Button size="small" onClick={handleNext} disabled={activeStep === maxSteps - 1}>
+                        Next
+                        {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+                    </Button>
+                }
+                backButton={
+                    <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+                        {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+                        Back
+                    </Button>
+                }
+            />
         </div>
-    )
+    );
 }
 
 export default Home;
